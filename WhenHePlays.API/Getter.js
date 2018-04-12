0d382351d2cbf9c0ad40551ad776eedeed149eb0
Aram Zhamkochyan <aramzham@hotmail.com>
2018-04-12 18:48:59 +0400

    split screen version

--- WhenHePlays.API/Getter.js
+++ WhenHePlays.API/Getter.js
@@ -1,15 +1,11 @@
 function GetMatch() {
-    $.get("http://localhost:54736/API/ARSENAL", function (data, s) {
-        var div = document.getElementById("center");
-        div.innerHTML += data.trim();
-        div.appendChild(div.firstChild);
-        document.getElementsByClassName("card__footer")[0].remove();
-        document.getElementsByClassName("fixture-match__versus")[0].remove();
-        var teams = document.getElementsByClassName("team-crest");
-        teams[0].id = "home";
-        teams[1].id = "away";
+    $.get("http://localhost:54736/api/arsenal", function (data, s) {
+        var centered = document.getElementsByClassName("centered");
+        centered[0].getElementsByTagName('img')[0].src = data[0];
+        centered[1].getElementsByTagName('img')[0].src = data[1];
+        centered[0].getElementsByTagName('h2')[0].innerText = data[2];
+        centered[1].getElementsByTagName('h2')[0].innerText = data[3];
     });
 }
 
 GetMatch();
-
--- WhenHePlays.API/Parser/ArsenalPasrser.cs
+++ WhenHePlays.API/Parser/ArsenalPasrser.cs
@@ -10,17 +10,23 @@ namespace Parser
         HttpClient _httpClient =new HttpClient();
         HtmlAgilityPack.HtmlDocument document=new HtmlDocument();
         private const string url = @"https://www.arsenal.com/";
-        public async Task<string> GetContent()
+        public async Task<string[]> GetContent()
         {
+            var result = new string[4];
             var content = await _httpClient.GetStringAsync(url);
             document.LoadHtml(content);
             var article = document.DocumentNode.SelectSingleNode(".//article[@class='views-element-container']");
-            var imgs = article.SelectNodes(".//img");
-            foreach (var img in imgs)
+            var imgs = article.SelectNodes(".//figure//img");
+            for (var i = 0; i < imgs.Count; i++)
             {
-                img.Attributes["src"].Value = img.Attributes["src"].Value.Insert(0, "https://www.arsenal.com");
+                result[i] = imgs[i].Attributes["src"].Value.Insert(0, "https://www.arsenal.com");
             }
-            return article.OuterHtml;
+            var teamNames = article.SelectNodes(".//div[@class='team-crest__name-value']");
+            for (var i = 0; i < teamNames.Count; i++)
+            {
+                result[i + 2] = teamNames[i].InnerText.Trim(' ', '\n', '\r', '\t');
+            }
+            return result;
         }
     }
 }
