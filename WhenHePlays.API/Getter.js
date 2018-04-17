72046c45681757fb3129dc2a7de9c466ad31c80f
Aram Zhamkochyan <aramzham@hotmail.com>
2018-04-17 17:12:54 +0400

    time and venue fixed, trying to add lineups before match message

--- WhenHePlays.API/Getter.js
+++ WhenHePlays.API/Getter.js
@@ -1,3 +1,21 @@
+var months = {
+    "Jan": 1,
+    "Feb": 2,
+    "Mar": 3,
+    "Apr": 4,
+    "May": 5,
+    "Jun": 6,
+    "Jul": 7,
+    "Aug": 8,
+    "Sep": 9,
+    "Oct": 10,
+    "Nov": 11,
+    "Dec": 12
+};
+
+var startDay = 0;
+var startTime = [0, 0];
+
 function GetMatch() {
     $.get("http://localhost:54736/api/arsenal", function (data, s) {
         var centered = document.getElementsByClassName("centered");
@@ -6,8 +24,17 @@ function GetMatch() {
         centered[0].getElementsByTagName('h2')[0].innerText = data[2];
         centered[1].getElementsByTagName('h2')[0].innerText = data[3];
 
-        document.getElementsByClassName('startDate')[0].innerText = data[4] + " | " + data[5];
+        document.getElementsByClassName('startDate')[0].innerText = _toArmTime(data[4]) + " | " + data[5];
     });
+}
+
+function _toArmTime(date) {
+    var spl = date.split(' ');
+    var time = spl[4].split(':');
+    startDay = spl[2];
+    startTime[0] = Number(time[0]) + 3;
+    startTime[1] = Number(time[1]);
+    return  spl[2] + "/" + months[spl[1]] + "/" + (new Date()).getFullYear().toString() + " - " + (Number(time[0]) + 3).toString() + ":" + time[1];
 }
 
 GetMatch();
--- WhenHePlays.API/init.js
+++ WhenHePlays.API/init.js
@@ -3,6 +3,9 @@
     function init() {
         var modal = document.getElementById('myModal');
         document.getElementById("startingLineups").onclick = function () {
+            if (startDay != new Date().getDay() ||
+            (new Date() - new Date(new Date().getFullYear(), new Date().getMonth(), startDay, startTime[0], startTime[1]) >=
+                3600000)) return; // return something, f.e. no lineup info before 1 hour
             modal.style.display = "block";
         };
 
